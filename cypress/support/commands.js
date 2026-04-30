666fe55a80fa79ab6dacd08f332a9a035850373f
Matheus Marangon <matheus.bmarangon@gmail.com>
2026-04-29 21:09:25 -0300

    Update commands.js
    Atualização de comentário

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,4 @@
-// adiciona um comando customizado ao Cypress
-// este comando permite reutilizar a navegação para a página inicial
+// comando para reutilizar a navegação para a página inicial
 Cypress.Commands.add("visitHome", () => {
 
   // acessa a rota raiz da aplicação (home)
